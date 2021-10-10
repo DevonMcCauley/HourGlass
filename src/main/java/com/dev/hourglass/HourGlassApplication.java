3b1dec132da99dc25d7b85f389c0a957161a04bc
Devon McCauley <54080684+DevonMcCauley@users.noreply.github.com>
2021-10-09 21:12:44 -0400

    Implement Spring Security

--- src/main/java/com/dev/hourglass/HourGlassApplication.java
+++ src/main/java/com/dev/hourglass/HourGlassApplication.java
@@ -4,7 +4,7 @@ package com.dev.hourglass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class HourGlassApplication {
 
     public static void main(String[] args) {
